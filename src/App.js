da6a5f074db6a09b0cd3368f5d7dd9e783976164
Christilato <christilato@gmail.com>
2022-11-02 10:25:30 -0500

    changed App js file accordingly

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/nav/Nav"
+import Header from "./components/header/Header"
+import About from "./components/about/About"
+import Footer from "./components/footer/Footer"
+import Contact from "./components/contact/Contact"
+import Portfolio from "./components/portfolio/Portfolio"
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    return (
+        <div>
+            <Header />
+            <Nav />
+            <About />
+            <Portfolio />
+            <Contact />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -7,3 +7,5 @@ function Portfolio () {
         </div>
     );
 };
+
+export default Portfolio;
